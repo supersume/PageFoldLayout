3c238dfc782b9cc1d829dbce933192cc9580bc37
Augustino Bildad <tinodady33@yahoo.com>
2017-12-06 09:57:30 +0300

    Add project files.

---
+++ PageFoldLayout/PageFoldLayout.cs
@@ -0,0 +1,216 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Util;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PageFoldLayout
+{
+    public class PageFoldLayout : FrameLayout
+    {
+        private int mWidth, mHeight;
+        private int HEIGHT_LIMIT;
+        private float mPointX;
+        private float mPointY;
+        private float mDegree;
+        private float mPercent = 0.0f;
+        private bool isShort;
+        //private Paint mPaint;
+        private Path mPath;
+        private Path mPathFoldandNext;
+
+        public PageFoldLayout(Context context) : base(context) { }
+
+        public PageFoldLayout(Context context, IAttributeSet attrs) : base(context, attrs) { }
+
+        public PageFoldLayout(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr) { }
+
+        private float BezierP(float p1, float p2, float p3, float t)
+        {
+            return (1 - t) * (1 - t) * p1 + 2 * (1 - t) * t * p2 + t * t * p3;
+        }
+
+        public void UpdatePoint(float percent)
+        {
+            mPercent = percent;
+            if (percent < 0)
+            {
+                percent += 1;
+            }
+            mPointX = BezierP(0f, mWidth, (mWidth << 1), percent);
+            mPointY = BezierP(mHeight, HEIGHT_LIMIT, mHeight, percent);
+            //Log.d("kanna", "update " + mPointX + " " + mPointY + " " + percent + " " + angle);
+            Invalidate();
+        }
+
+        private void InitPaint()
+        {
+            /*
+            mPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.DITHER_FLAG);
+            mPaint.setStyle(Paint.Style.STROKE);
+            mPaint.setStrokeWidth(10);*/
+
+            mPath = new Path();
+            mPathFoldandNext = new Path();
+        }
+
+        protected void OnSizeChanged(int w, int h, int oldw, int oldh)
+        {
+            InitPaint();
+            mWidth = w;
+            mHeight = h;
+            HEIGHT_LIMIT = h >> 1;
+        }
+
+        private void DrawView(Canvas canvas)
+        {
+            if (mPercent > 0)
+            {
+                //upper region
+                canvas.Save();
+                canvas.ClipRect(0, 0, mWidth, mHeight);
+                if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+                {
+                    canvas.ClipPath(mPathFoldandNext, Region.Op.Difference);
+                }
+                else
+                {
+                    canvas.ClipOutPath(mPathFoldandNext);
+                }
+                //canvas.drawColor(Color.WHITE);
+                base.DispatchDraw(canvas);
+                canvas.Restore();
+
+
+                //fold region
+                canvas.Save();
+                canvas.ClipPath(mPath);
+                //canvas.clipPath(mPathFoldandNext);
+
+                //cos@ = sizeShort - mPointX / sizeShort
+                if (isShort)
+                {
+                    canvas.Translate(mPointX, mPointY);
+                    canvas.Rotate(mDegree);
+                    canvas.Scale(1, -1);
+                    canvas.Translate(0, -mHeight);
+                }
+                //sin@ = mHeight - mPointY / sizeShort
+                else
+                {
+                    canvas.Translate(mPointX, mPointY);
+                    canvas.Rotate(-mDegree);
+                    canvas.Scale(-1, 1);
+                    canvas.Translate(0, -mHeight);
+                }
+
+
+                base.DispatchDraw(canvas);
+                //canvas.drawColor(Color.WHITE);
+                canvas.Restore();
+            }
+            else
+            {
+                canvas.Save();
+                canvas.ClipPath(mPathFoldandNext);
+                //canvas.clipRegion(regionFold, Region.Op.DIFFERENCE);
+                if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+                {
+                    canvas.ClipPath(mPath, Region.Op.Difference);
+                }
+                else
+                {
+                    canvas.ClipOutPath(mPath);
+                }
+                base.DispatchDraw(canvas);
+                canvas.Restore();
+            }
+        }
+
+        private void DispatchDrawUsingBitmap(Canvas canvas)
+        {
+            mPath.Reset();
+            mPathFoldandNext.Reset();
+            canvas.DrawColor(Color.Transparent);
+
+            float TX = mHeight - mPointY;
+            float OX = mPointX;
+            float temp = (float)(Math.Pow(TX, 2) + Math.Pow(OX, 2));
+
+            float sizeShort = temp / (2 * OX);
+            float sizeLong = temp / (2 * TX);
+
+            if (sizeShort > sizeLong)
+            {
+                isShort = true;
+                float sin = (sizeShort - mPointX) / sizeShort;
+                mDegree = (float)(Math.Acos(sin) / Math.PI * 180);
+                //Log.d("kanna","get "+ mDegree);
+            }
+            else
+            {
+                isShort = false;
+                float sin = (mHeight - mPointY) / sizeShort;
+                mDegree = (float)(Math.Asin(sin) / Math.PI * 180);
+            }
+
+
+            mPath.MoveTo(mPointX, mPointY);
+            mPathFoldandNext.MoveTo(0, mHeight);
+
+            if (sizeLong > mHeight)
+            {
+                float an = sizeLong - mHeight;
+                float nm = an / (sizeLong - TX) * OX;
+                float nq = an / sizeLong * sizeShort;
+                mPath.LineTo(nm, 0);
+                mPath.LineTo(nq, 0);
+                mPath.LineTo(sizeShort, mHeight);
+                mPath.Close();
+
+                mPathFoldandNext.LineTo(0, 0);
+                mPathFoldandNext.LineTo(nm, 0);
+                mPathFoldandNext.LineTo(mPointX, mPointY);
+                mPathFoldandNext.LineTo(sizeShort, mHeight);
+                mPathFoldandNext.Close();
+
+
+            }
+            else
+            {
+                mPath.LineTo(0, mHeight - sizeLong);
+                mPath.LineTo(sizeShort, mHeight);
+                mPath.Close();
+
+
+                mPathFoldandNext.LineTo(0, mHeight - sizeLong);
+                mPathFoldandNext.LineTo(mPointX, mPointY);
+                mPathFoldandNext.LineTo(sizeShort, mHeight);
+                mPathFoldandNext.Close();
+            }
+            DrawView(canvas);
+            /*
+            mPaint.setColor(Color.BLACK);
+            canvas.drawPath(mPath, mPaint);
+            mPaint.setColor(Color.RED);
+            canvas.drawPath(mPathFoldandNext, mPaint);*/
+        }
+
+        protected override void DispatchDraw(Canvas canvas)
+        {
+            if (Width <= 0 || Height <= 0 || mPercent == 0)
+            {
+                base.DispatchDraw(canvas);
+            }
+            else
+            {
+                DispatchDrawUsingBitmap(canvas);
+            }
+            
+        }
+    }
+}
---
+++ PageFoldLayout/PageFoldTransformer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V4.View;
+
+namespace PageFoldLayout
+{
+    public class PageFoldTransformer : Java.Lang.Object, ViewPager.IPageTransformer
+    {
+        public void TransformPage(View view, float position)
+        {
+            int width = view.Width;
+            if (position < -1)
+            { // [-Infinity,-1)
+              // This page is way off-screen to the left.
+                view.TranslationX = 0;
+
+            }
+            else if (position <= 0)
+            { // [-1,0]
+              //Log.d("kanna",view.getTag().toString() + " position " + position);
+                if (position != -1)
+                {
+                    ((PageFoldLayout)view).UpdatePoint(position);
+                    view.TranslationX = (-position * width);
+                }
+                else
+                {
+                    view.TranslationX = 0;
+                }
+            }
+            else if (position <= 1)
+            { // (0,1]
+              //Log.d("kanna",view.getTag().toString()+ " position " + position);
+                if (position != 1)
+                {
+                    ((PageFoldLayout)view).UpdatePoint(position);
+                    view.TranslationX = (-position * width);
+                }
+                else
+                {
+                    view.TranslationX = (0);
+                }
+            }
+            else
+            { // (1,+Infinity]
+              // This page is way off-screen to the right.
+                view.TranslationX = 0;
+            }
+        }
+    }
+}
